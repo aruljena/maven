efddf7343b4dcb686cb51c0af5c7d20e67ba9a4c
aruljena <98793513+aruljena@users.noreply.github.com>
2022-02-01 17:02:05 +0530

    this is new maven

---
+++ BankInformation/src/org/bank/AxisBank.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	private void deposit() {
+		System.out.println("Deposit bank");
+
+	}
+	public static void main(String[] args) {
+		
+	AxisBank a = new AxisBank();
+	a.deposit();
+	a.saving();
+	a.fixing();
+		
+	}
+
+}
---
+++ BankInformation/src/org/bank/BankInfo.java
@@ -0,0 +1,12 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("Saving a amount");
+
+	}
+	public void fixing() {
+		System.out.println("Fixing a amount");
+	}
+
+}
